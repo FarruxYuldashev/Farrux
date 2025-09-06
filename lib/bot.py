1a72e87b5e4c08514c74b935d1879c60cc9a36d9
Farrux Yuldashev <sening_emailing@example.com>
2025-09-06 23:27:13 +0500

    Update mini app code

--- lib/bot.py
+++ lib/bot.py
@@ -1,14 +1,16 @@
 import telebot
 from telebot import types
 
-TOKEN = "BOT_TOKENINGNI_QO'Y"
+TOKEN = "7958495384:AAGxdSN3-oTv5QgO5mYaKgeYYPLXzwwOO-0"
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.InlineKeyboardMarkup()
-    webAppTest = types.WebAppInfo("https://SENING_WEB_APP_LINKING")  # Flutter web app linki
+    # Flutter Web app linkini shu yerga qo'yamiz
+    webAppTest = types.WebAppInfo("https://demodversion.netlify.app/")
     markup.add(types.InlineKeyboardButton("Mini App ochish 🚀", web_app=webAppTest))
     bot.send_message(message.chat.id, "Salom! Mini appni oching 👇", reply_markup=markup)
 
 bot.polling()
+
